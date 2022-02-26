fb6edeaa16bf70ac8cdd513d498aa43abb410c86
Egor) <kes2079531@gmail.com>
2022-02-26 22:34:07 +0300

    fixed JsonPath

--- src/test/java/ru/netology/post/MobileAppPostmanEchoTest.java
+++ src/test/java/ru/netology/post/MobileAppPostmanEchoTest.java
@@ -19,6 +19,6 @@ public class MobileAppPostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("some date"));
+                .body("data", equalTo("some data"));
     }
 }
